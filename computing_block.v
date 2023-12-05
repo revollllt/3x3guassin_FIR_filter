ca7e66e6ae32cece13df4bd601589b071692f101
zou <zou@zou.com>
2023-12-05 20:00:38 +0800

    fix: extend coff

--- computing_block.v
+++ computing_block.v
@@ -23,23 +23,9 @@ output [DATA_WIDTH-1:0] data_out
 //    parameter         corner_flag_lastline  = 7;
 //    parameter         total_cross           = 8;
 
-reg [4:0]   coff_0  [2:0];  // a sign_bit is added to the front of the number
-reg [4:0]   coff_1  [2:0];
-reg [4:0]   coff_2  [2:0];
-
-
-// guassin kernal
-//     [1 2 1]
-// 1/16[2 4 2]
-//     [1 2 1]
-
-// [-1 0 -1]
-// [ 0 4  0]
-// [-1 0 -1]
-
-// [0 -1 -2]
-// [1  0 -1]
-// [2  1  0]
+reg [3:0]   coff_0  [2:0];
+reg [3:0]   coff_1  [2:0];
+reg [3:0]   coff_2  [2:0];
 
 always@(*) begin
 case (corner_type)
@@ -55,11 +41,11 @@ case (corner_type)
         coff_2[2] <= 0;
         end
 4'd1 :  begin           // start//
-        coff_0[0] <= 0;
-        coff_0[1] <= 1;
+        coff_0[0] <= 1;
+        coff_0[1] <= 2;
         coff_0[2] <= 0;
-        coff_1[0] <= 5'b10001;
-        coff_1[1] <= 0;
+        coff_1[0] <= 2;
+        coff_1[1] <= 4;
         coff_1[2] <= 0;
         coff_2[0] <= 0;
         coff_2[1] <= 0;
@@ -67,46 +53,46 @@ case (corner_type)
         end
 4'd2 :  begin           // right start//
         coff_0[0] <= 0;
-        coff_0[1] <= 1;
-        coff_0[2] <= 2;
+        coff_0[1] <= 2;
+        coff_0[2] <= 1;
         coff_1[0] <= 0;
-        coff_1[1] <= 0;
-        coff_1[2] <= 1;
+        coff_1[1] <= 4;
+        coff_1[2] <= 2;
         coff_2[0] <= 0;
         coff_2[1] <= 0;
         coff_2[2] <= 0;
         end
 4'd3 :  begin           // the left beginning//
-        coff_0[0] <= 0;
-        coff_0[1] <= 1;
+        coff_0[0] <= 1;
+        coff_0[1] <= 2;
         coff_0[2] <= 0;
-        coff_1[0] <= 5'b10001;
-        coff_1[1] <= 0;
+        coff_1[0] <= 2;
+        coff_1[1] <= 4;
         coff_1[2] <= 0;
-        coff_2[0] <= 5'b10010;
-        coff_2[1] <= 5'b10001;
+        coff_2[0] <= 1;
+        coff_2[1] <= 2;
         coff_2[2] <= 0;
         end
 4'd4 :  begin           // the right boarder//
         coff_0[0] <= 0;
-        coff_0[1] <= 1;
-        coff_0[2] <= 2;
+        coff_0[1] <= 2;
+        coff_0[2] <= 1;
         coff_1[0] <= 0;
-        coff_1[1] <= 0;
-        coff_1[2] <= 1;
+        coff_1[1] <= 4;
+        coff_1[2] <= 2;
         coff_2[0] <= 0;
-        coff_2[1] <= 5'b10001;
-        coff_2[2] <= 0;
+        coff_2[1] <= 2;
+        coff_2[2] <= 1;
         end
 4'd5 :  begin           // the leftend//
         coff_0[0] <= 0;
         coff_0[1] <= 0;
         coff_0[2] <= 0;
-        coff_1[0] <= 5'b10001;
-        coff_1[1] <= 0;
+        coff_1[0] <= 2;
+        coff_1[1] <= 4;
         coff_1[2] <= 0;
-        coff_2[0] <= 5'b10010;
-        coff_2[1] <= 5'b10001;
+        coff_2[0] <= 1;
+        coff_2[1] <= 2;
         coff_2[2] <= 0;
         end
 4'd6 :  begin           // the rigntend//
@@ -114,112 +100,121 @@ case (corner_type)
         coff_0[1] <= 0;
         coff_0[2] <= 0;
         coff_1[0] <= 0;
-        coff_1[1] <= 0;
-        coff_1[2] <= 1;
+        coff_1[1] <= 4;
+        coff_1[2] <= 2;
         coff_2[0] <= 0;
-        coff_2[1] <= 5'b10001;
-        coff_2[2] <= 0;
+        coff_2[1] <= 2;
+        coff_2[2] <= 1;
         end
 default: begin           // the total 3*3//
-        coff_0[0] <= 0;
-        coff_0[1] <= 1;
-        coff_0[2] <= 2;
-        coff_1[0] <= 5'b10001;
-        coff_1[1] <= 0;
-        coff_1[2] <= 1;
-        coff_2[0] <= 5'b10010;
-        coff_2[1] <= 5'b10001;
-        coff_2[2] <= 0;
+        coff_0[0] <= 1;
+        coff_0[1] <= 2;
+        coff_0[2] <= 1;
+        coff_1[0] <= 2;
+        coff_1[1] <= 4;
+        coff_1[2] <= 2;
+        coff_2[0] <= 1;
+        coff_2[1] <= 2;
+        coff_2[2] <= 1;
         end
 endcase
 end 
 
-wire   [aba_adder_WIDTH+sign_bit-1:0] data_out_r;
-localparam aba_adder_WIDTH = 16;
-localparam sign_bit = 1;
-wire   [aba_adder_WIDTH+sign_bit-1:0] mul_out [8:0];
-wire   [aba_adder_WIDTH+sign_bit-1:0] complement_code [8:0];
+wire   [11:0] data_out_r;
+localparam aba_adder_WIDTH = 12;
+wire   [aba_adder_WIDTH-1:0] mul_out [8:0];
+
 // store multiply answer in mul_out 
+
+// assign mul_out[0] = line0_data0*coff_0[0];
+// assign mul_out[1] = line0_data1*coff_0[1];
+// assign mul_out[2] = line0_data2*coff_0[2];
+// assign mul_out[3] = line1_data0*coff_1[0];
+// assign mul_out[4] = line1_data1*coff_1[1];
+// assign mul_out[5] = line1_data2*coff_1[2];
+// assign mul_out[6] = line2_data0*coff_2[0];
+// assign mul_out[7] = line2_data1*coff_2[1];
+// assign mul_out[8] = line2_data2*coff_2[2];
 
 // dadda_tree
 wire [15:0] dadda_result [8:0];
 
-genvar k;
-generate
-    begin:COMPLEMENT_ADDER
-        for (k=0; k<9; k=k+1) begin
-                aba_adder aba_adder_complement(
-                        .cin(1'b1),
-                        .a(~{1'b0,dadda_result[k]}),
-                        .b(17'b0),
-                        .s(complement_code[k]),
-                        .cout()
-                );
-        end
-    end
-endgenerate
 
-assign mul_out[0] = (coff_0[0][4]==1) ? complement_code[0] : {1'b0,dadda_result[0]};
-assign mul_out[1] = (coff_0[1][4]==1) ? complement_code[1] : {1'b0,dadda_result[1]};
-assign mul_out[2] = (coff_0[2][4]==1) ? complement_code[2] : {1'b0,dadda_result[2]};
-assign mul_out[3] = (coff_1[0][4]==1) ? complement_code[3] : {1'b0,dadda_result[3]};
-assign mul_out[4] = (coff_1[1][4]==1) ? complement_code[4] : {1'b0,dadda_result[4]};
-assign mul_out[5] = (coff_1[2][4]==1) ? complement_code[5] : {1'b0,dadda_result[5]};
-assign mul_out[6] = (coff_2[0][4]==1) ? complement_code[6] : {1'b0,dadda_result[6]};
-assign mul_out[7] = (coff_2[1][4]==1) ? complement_code[7] : {1'b0,dadda_result[7]};
-assign mul_out[8] = (coff_2[2][4]==1) ? complement_code[8] : {1'b0,dadda_result[8]};
+assign mul_out[0] = dadda_result[0][aba_adder_WIDTH+3:4];
+assign mul_out[1] = dadda_result[1][aba_adder_WIDTH+3:4];
+assign mul_out[2] = dadda_result[2][aba_adder_WIDTH+3:4];
+assign mul_out[3] = dadda_result[3][aba_adder_WIDTH+3:4];
+assign mul_out[4] = dadda_result[4][aba_adder_WIDTH+3:4];
+assign mul_out[5] = dadda_result[5][aba_adder_WIDTH+3:4];
+assign mul_out[6] = dadda_result[6][aba_adder_WIDTH+3:4];
+assign mul_out[7] = dadda_result[7][aba_adder_WIDTH+3:4];
+assign mul_out[8] = dadda_result[8][aba_adder_WIDTH+3:4];
+
+wire [7:0] coff0 [2:0];
+wire [7:0] coff1 [2:0];
+wire [7:0] coff2 [2:0];
+
+assign coff0[0] = coff_0[0] << 4;
+assign coff0[1] = coff_0[1] << 4;
+assign coff0[2] = coff_0[2] << 4;
+assign coff1[0] = coff_1[0] << 4;
+assign coff1[1] = coff_1[1] << 4;
+assign coff1[2] = coff_1[2] << 4;
+assign coff2[0] = coff_2[0] << 4;
+assign coff2[1] = coff_2[1] << 4;
+assign coff2[2] = coff_2[2] << 4;
 
 dadda_tree dadda_tree_0(
         .m1(line0_data0),
-        .m2({{0,0,0,0},coff_0[0][3:0]}),
+        .m2(coff0[0]),
         .dadda_result(dadda_result[0])
 );
 dadda_tree dadda_tree_1(
         .m1(line0_data1),
-        .m2({{0,0,0,0},coff_0[1][3:0]}),
+        .m2(coff0[1]),
         .dadda_result(dadda_result[1])
 );
 dadda_tree dadda_tree_2(
         .m1(line0_data2),
-        .m2({{0,0,0,0},coff_0[2][3:0]}),
+        .m2(coff0[2]),
         .dadda_result(dadda_result[2])
 );
 dadda_tree dadda_tree_3(
         .m1(line1_data0),
-        .m2({{0,0,0,0},coff_1[0][3:0]}),
+        .m2(coff1[0]),
         .dadda_result(dadda_result[3])
 );
 dadda_tree dadda_tree_4(
         .m1(line1_data1),
-        .m2({{0,0,0,0},coff_1[1][3:0]}),
+        .m2(coff1[1]),
         .dadda_result(dadda_result[4])
 );
 dadda_tree dadda_tree_5(
         .m1(line1_data2),
-        .m2({{0,0,0,0},coff_1[2][3:0]}),
+        .m2(coff1[2]),
         .dadda_result(dadda_result[5])
 );
 dadda_tree dadda_tree_6(
         .m1(line2_data0),
-        .m2({{0,0,0,0},coff_2[0][3:0]}),
+        .m2(coff2[0]),
         .dadda_result(dadda_result[6])
 );
 dadda_tree dadda_tree_7(
         .m1(line2_data1),
-        .m2({{0,0,0,0},coff_2[1][3:0]}),
+        .m2(coff2[1]),
         .dadda_result(dadda_result[7])
 );
 dadda_tree dadda_tree_8(
         .m1(line2_data2),
-        .m2({{0,0,0,0},coff_2[2][3:0]}),
+        .m2(coff2[2]),
         .dadda_result(dadda_result[8])
 );
 
 
 
-wire [aba_adder_WIDTH+sign_bit-1:0] ADD_OUT_firstlayer  [3:0];
-wire [aba_adder_WIDTH+sign_bit-1:0] ADD_OUT_secondlayer [1:0];
-wire [aba_adder_WIDTH+sign_bit-1:0] ADD_OUT_thirdlayer;
+wire [aba_adder_WIDTH-1:0] ADD_OUT_firstlayer  [3:0];
+wire [aba_adder_WIDTH-1:0] ADD_OUT_secondlayer [1:0];
+wire [aba_adder_WIDTH-1:0] ADD_OUT_thirdlayer;
 
 // -------------connet adder (like a pyramid)------------- //
 // 0+4   1+5   2+6   3+7   8        firstlayer
@@ -268,7 +263,7 @@ aba_adder aba_adder_fourthlayer(
         .cout()
 );
 
-wire [aba_adder_WIDTH-1:0] data_out_rb;
-assign data_out_rb  =  (data_out_r[aba_adder_WIDTH+sign_bit-1]==1) ? 0 :  data_out_r;
-assign data_out     =    data_out_rb[DATA_WIDTH-1:0];
+wire [11:0] data_out_rb;
+assign data_out_rb  =    data_out_r >> 4;
+assign data_out    =    data_out_rb[DATA_WIDTH-1:0];
 endmodule
--- improve_adder.v
+++ improve_adder.v
@@ -1,4 +1,4 @@
-module aba_adder #(parameter WIDTH=17, LCA_WIDTH = 4)(
+module aba_adder #(parameter WIDTH=12, LCA_WIDTH = 4)(
     input  cin,
     input  [WIDTH-1:0] a,
     input  [WIDTH-1:0] b,
@@ -6,16 +6,18 @@ module aba_adder #(parameter WIDTH=17, LCA_WIDTH = 4)(
     output cout
 );
 
-wire [4:0] c;
-wire [3:0] s0;
-wire [LCA_WIDTH-1:0] s1,s2,s3;
-wire s4s;
+wire [2:0] c;
+wire [WIDTH-LCA_WIDTH-LCA_WIDTH-1:0] s0;
+wire [LCA_WIDTH-1:0] s1;
+wire [LCA_WIDTH-1:0] s2;
+
 // Omit the first four digits
-// assign s0 = {a[3],a[3],a[3],a[3]};
-// assign c[0] = a[3];
+assign s0 = {a[3],a[3],a[3],a[3]};
+assign c[0] = a[3];
 
-assign cout = c[4];
+assign cout = c[2];
 
+/*
 LCA4bit LCA4bit_test(
     .cin(cin),
     .a(a[3:0]),
@@ -23,37 +25,23 @@ LCA4bit LCA4bit_test(
     .s(s0),
     .cout(c[0])
 );
+*/
 
 
 LCA4bit LCA4bit_0(
     .cin(c[0]),
-    .a(a[7:4]),
-    .b(b[7:4]),
+    .a(a[WIDTH-1-LCA_WIDTH:WIDTH-LCA_WIDTH-LCA_WIDTH]),
+    .b(b[WIDTH-1-LCA_WIDTH:WIDTH-LCA_WIDTH-LCA_WIDTH]),
     .s(s1),
     .cout(c[1])
 );
 LCA4bit LCA4bit_1(
     .cin(c[1]),
-    .a(a[11:8]),
-    .b(b[11:8]),
+    .a(a[WIDTH-1:WIDTH-LCA_WIDTH]),
+    .b(b[WIDTH-1:WIDTH-LCA_WIDTH]),
     .s(s2),
     .cout(c[2])
 );
-LCA4bit LCA4bit_2(
-    .cin(c[2]),
-    .a(a[15:12]),
-    .b(b[15:12]),
-    .s(s3),
-    .cout(c[3])
-);
-FA FA_out(
-    .a(a[WIDTH-1]),
-    .b(b[WIDTH-1]),
-    .cin(c[3]),
-    .s(s4),
-    .cout(c[4])
-);
-
-assign s = {s4,s3,s2,s1,s0};
+assign s = {s2,s1,s0};
 
 endmodule
